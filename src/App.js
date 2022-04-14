80645087b3024ff39ce68e328c26f69fc20572b1
Erbol Nurekov <64440211+nrkvv04@users.noreply.github.com>
2022-04-15 01:07:45 +0600

    asdfasdf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import Header from "./Components/Header/Header";
-import "./App.css"
 import About from "./Components/About/About";
+import Footer from "./Components/Footer/Footer";
+import React, {useState} from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {EffectCoverflow, Pagination} from "swiper";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "./App.css"
 
 
 function App() {
+
     return (
         <div className="App">
-                <Header/>
-                <About/>
+            <Header/>
+
+            <Swiper
+                effect={"coverflow"}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                modules={[EffectCoverflow, Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <About/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <About/>
+                </SwiperSlide>
+
+            </Swiper>
+
+
+            <Footer/>
         </div>
     );
 }
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import "./About.css";
-import firebase from "./images/firebase.png"
-import webapp from "./images/webapp.png"
-import react from "./images/react.png"
-import js from "./images/js.png"
 
 const About = () => {
     return (
@@ -14,30 +10,13 @@ const About = () => {
                     <div className="aboutRow__left">
                         <h2 className="aboutRow__left_title">HELLO</h2>
                         <h2 className="aboutRow__left_subtitle">I'M Erbol Nurekov</h2>
-                        <span className="aboutRow__left_info">I've been doing web design, front-end and back-end development
-                            for a year now.Do you need a website design, site layout, or maybe
-                            a turnkey website? Then contact me</span>
+                        <span className="aboutRow__left_info">I am a frontend developer who loves his job, loves to develop interesting products and a person who loves to study interesting and useful technologies</span>
                         <button className="aboutRow__left_btn">CONTACT ME</button>
                     </div>
 
                     <div className="aboutRow__right">
                         <div className="aboutRow__right_circle">
-                            <span className="aboutRow__right_circle_react">
-                                REACT
-                                <img className="aboutRow__right_circle_firebase-logo" src={react} alt="firebase icon"/>
-                            </span>
-                            <span className="aboutRow__right_circle_firebase">
-                                FIREBASE
-                                <img className="aboutRow__right_circle_firebase-logo" src={firebase} alt="firebase icon"/>
-                            </span>
-                            <span className="aboutRow__right_circle_js">
-                                JS
-                                <img className="aboutRow__right_circle_firebase-logo" src={js} alt="firebase icon"/>
-                            </span>
-                            <span className="aboutRow__right_circle_webapp">
-                                WEB-Application
-                                <img className="aboutRow__right_circle_firebase-logo" src={webapp} alt="firebase icon"/>
-                            </span>
+                            <img src="" alt=""/>
                         </div>
                     </div>
 

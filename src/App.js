e262d7f6d3673d2f0e7696cb22a387abd0e8492e
Erbol Nurekov <64440211+nrkvv04@users.noreply.github.com>
2022-04-16 01:49:38 +0600

    changed swipe

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import React, {useState} from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
 
 import "swiper/css";
-import "swiper/css/effect-flip";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { EffectFlip, Pagination, Navigation } from "swiper";
 
 
 import "./App.css"
@@ -22,8 +18,6 @@ function App() {
             <Header/>
 
             <Swiper
-                effect={"flip"}
-                modules={[EffectFlip, Pagination, Navigation]}
                 className="mySwiper"
             >
                 <SwiperSlide>
